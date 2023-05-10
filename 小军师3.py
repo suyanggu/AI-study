6f982e7667e78fa96930cce1f10e867d7505fc23
Yolanda Su <69511013+suyanggu@users.noreply.github.com>
2023-05-10 23:14:22 +0800

    Create 小军师3.py

---
+++ 小军师3.py
@@ -0,0 +1,110 @@
+import openai
+import docx
+import configparser
+
+
+# 从ini文件中读取api_key
+openai.api_key = "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"
+
+doc = docx.Document("/Users/long/Desktop/小军师-效能测试数据.docx")
+text = "\n".join([paragraph.text for paragraph in doc.paragraphs])
+
+# Function to send a message to the OpenAI chatbot model and return its response
+# 发送消息给OpenAI的聊天机器人模型，并返回它的回复
+def send_message(message_log):
+    # Use OpenAI's ChatCompletion API to get the chatbot's response
+    # 使用OpenAI的ChatCompletion API来获取聊天机器人的回复
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",  # The name of the OpenAI chatbot model to use
+        # 这个模型是网页版OpenAI chatbot的模型
+        # The conversation history up to this point, as a list of dictionaries
+        # 将目前为止的对话历史记录，作为字典的列表
+        messages=message_log,
+        # The maximum number of tokens (words or subwords) in the generated response
+        # 最大的生成回复的token数 最大4095（单词或子单词）
+        max_tokens=500,
+        # The stopping sequence for the generated response, if any (not used here)
+        # 停止生成回复的序列，如果有的话（这里没有使用）
+        stop=None,
+        # The "creativity" of the generated response (higher temperature = more creative)
+        # 生成回复的“创造性”（参数越高，创造性越强）
+        temperature=0.9,
+    )
+
+    # Find the first response from the chatbot that has text in it (some responses may not have text)
+    # 从聊天机器人的回复中，找到第一个有文本的回复（有些回复可能没有文本）
+    for choice in response.choices:
+        if "text" in choice:
+            return choice.text
+
+    # If no response with text is found, return the first response's content (which may be empty)
+    # 如果没有找到有文本的回复，返回第一个回复的内容（可能为空）
+    return response.choices[0].message.content
+
+
+# Main function that runs the chatbot
+def main():
+    # Initialize the conversation history with a message from the chatbot
+    # 用聊天机器人的消息来初始化对话历史记录
+    message_log = [
+        {"role": "system", "content": "You are a helpful assistant."}
+    ]
+
+    # Set a flag to keep track of whether this is the first request in the conversation
+    # 设置一个标志来跟踪这是否是对话中的第一个请求
+    first_request = True
+
+    # Start a loop that runs until the user types "quit"
+    # 开始一个循环，直到用户输入“quit”为止
+    while True:
+        if first_request:
+            # If this is the first request, get the user's input and add it to the conversation history
+            # 如果这是第一个请求，获取用户的输入，并将其添加到对话历史记录中
+            prompt = f"请结合下面的文本内容，根据你自己的经验知识，总结文本内容表现出的管理现状情况，并给出初步的管理建议。（管理现状和初步建议要换行区分，分别表达；管理现状和初步建议中用序号分开进行要点表达，但要点要尽量抽象概括，不要只是复述数据）不要再把数据复述一遍，直接给出概括性的结论。\
+    文本内容是：{text}\""
+            message_log.append({"role": "user", "content": prompt})
+
+            # Add a message from the chatbot to the conversation history
+            # 添加来自聊天机器人的消息到对话历史记录中
+            message_log.append(
+                {"role": "assistant", "content": "You are a helpful assistant."})
+
+            # Send the conversation history to the chatbot and get its response
+            # 发送对话历史记录给聊天机器人，并获取它的回复
+            response = send_message(message_log)
+
+            # Add the chatbot's response to the conversation history and print it to the console
+            # 添加聊天机器人的回复到对话历史记录中，并将其打印到控制台
+            message_log.append({"role": "assistant", "content": response})
+            print(f"一、根据目前提供的指标对现状进行分析，给出初步建议：\n   {response}\n\n二、行业经验表明，从「产品准确度」、「质量」、「交付速度」、「研发团队个人效能」几个维度对研发效能进行考察是最佳实践。您提供的指标尚不全面，建议在四个维度上补充更多指标，以帮助我站在更全面的视角为您提供更准确的现状分析和管理建议。下面是关于效能四维度的解释，以及各个维度的相关指标，并附带了详细的指标相关业务数据的搜集策略和统计方法。\n  1. 产品准确度\n产品准确度是指产品能否满足用户需求的程度。为了提高产品准确度，企业可以采用用户体验设计（UX）方法，进行用户研究、需求分析和交互设计等，以确保产品能够满足用户的期望。同时，对产品进行严格的质量控制和测试，以确保产品能够稳定运行并符合功能要求。\n相关指标模型统计方法和数据搜集策略：www.baidu.com\n\n2. 质量\n产品质量是指产品能够满足用户期望的程度。为了提高产品质量，企业可以采用质量管理（QM）方法，如Six Sigma和ISO 9001等，对产品的整个生命周期进行管理和控制，以确保产品的稳定性和可靠性。\n相关指标模型统计方法和数据搜集策略：www.baidu.com\n\n3. 交付速度\n交付速度是指企业向客户交付产品和服务的速度。为了提高交付速度，企业可以采用敏捷开发（Agile Development）和持续集成（Continuous Integration）等方法，以便更快地推出新产品和服务，并更快地满足客户需求。\n相关指标模型统计方法和数据搜集策略：www.baidu.com\n\n4. 研发团队个人效能\n研发团队个人效能是指团队成员在研发过程中的效率和能力。为了提高研发团队个人效能，企业可以采用各种培训和发展计划，以提高团队成员的技能和知识水平。此外，企业还可以通过激励计划和绩效评估，激励团队成员在研发过程中做出更好的表现。\n相关指标模型统计方法和数据搜集策略：www.baidu.com\n\n\n三、当您的团队在不断发展壮大时，如何高效地管理团队效能是至关重要的问题。ONES是一款高效的团队效能管理工具，它可以帮助您实现全方位的团队效能管理，包括任务管理、进度跟踪、协作沟通、绩效评估等多个方面。\n\n欲了解ONES更多信息，请移步官网：www.baidu.com")
+
+            # Set the flag to False so that this branch is not executed again
+            # 设置标志为False，以便不再执行此分支
+            first_request = False
+        else:
+            # If this is not the first request, get the user's input and add it to the conversation history
+            # 如果这不是第一个请求，获取用户的输入，并将其添加到对话历史记录中
+            user_input = input("You: ")
+
+            # If the user types "quit", end the loop and print a goodbye message
+            # 如果用户输入“quit”，结束循环并打印再见消息
+            if user_input.lower() == "quit":
+                print("Goodbye!")
+                break
+
+            message_log.append({"role": "user", "content": user_input})
+
+            # Send the conversation history to the chatbot and get its response
+            # 发送对话历史记录给聊天机器人，并获取它的回复
+            response = send_message(message_log)
+
+            # Add the chatbot's response to the conversation history and print it to the console
+            # 添加聊天机器人的回复到对话历史记录中，并将其打印到控制台
+            message_log.append({"role": "assistant", "content": response})
+            print(f"AI assistant: {response}")
+
+
+# Call the main function if this file is executed directly (not imported as a module)
+if __name__ == "__main__":
+    main()
+
